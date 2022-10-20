b8c0380a16557726c944b68284f68a26e838be5e
HtSpDuyTran <83074825+HtSpDuyTran@users.noreply.github.com>
2022-10-19 22:49:18 -0300

    test init

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/',(req, res) => {
+    res.end('<h1>dcdcd</h1>')
+})
+
+app.listen(port,hostname, () => {
+    console.log('dvidvmk')
+})
